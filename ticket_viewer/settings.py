773e9b0a59e5124f5de1dadc2f3305908744a7d4
comeonleo <wutexuanleo@outlook.com>
2019-06-12 21:42:59 +1000

    refine README.md

--- ticket_viewer/settings.py
+++ ticket_viewer/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = cfg.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ["45.113.233.241"]
+ALLOWED_HOSTS = ["127.0.0.1"]
 
 
 # Application definition
